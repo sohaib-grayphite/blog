8e73afb0f9506f3c0a9fc2ecd9cd8842af9a5cdd
“Muhammad <m.bilal@selectxstudio.com>
2021-09-24 13:57:24 +0500

    Revert "new addition"
    This reverts commit 25e6e52aa21d24a6d1d41d64b8bd17a283ea29b1.

--- backend/urls.py
+++ backend/urls.py
@@ -27,3 +27,5 @@ urlpatterns = [
     path("blog/",  BlogViews.as_view(), name='blog_views')
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG:
+    urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
